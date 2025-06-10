27e0bcea9cfe4d506b17fd19f1c65cbbb095d274
shehara gamage <sheharagamage2002@gmail.com>
2025-06-10 22:22:32 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+//type rafce
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default App
---
+++ src/context/AppContext.jsx
@@ -0,0 +1,22 @@
+
+import React, { createContext, useState, useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+export const AppContext = createContext();
+
+export const AppContextProvider = ({ children }) => {
+    const navigate = useNavigate();
+    const [user, setUser] = useState(null);
+    const [isSeller, setIsSeller] = useState(false);
+    const value = { navigate, user, setUser, isSeller, setIsSeller };
+
+    return (
+        <AppContext.Provider value={value}>
+            {children}
+        </AppContext.Provider>
+    );
+};
+
+export const useAppContext = () => {
+    return useContext(AppContext);
+};
