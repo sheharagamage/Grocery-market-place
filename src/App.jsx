580f89f9213726c1b562fdcede2a5a6025397846
shehara gamage <sheharagamage2002@gmail.com>
2025-06-20 10:55:49 +0530

    create All products pages

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,36 @@ import React from 'react'
 import Navbar from './components/navbar'
 import { Route,Routes, useLocation } from 'react-router-dom'
 import Home from './pages/Home'
+import { Toaster } from 'react-hot-toast'
+import Footer from './components/Footer'
+import { useAppContext } from './context/AppContext'
+import Login from './components/login'
+import AllProducts from './pages/AllProducts'
+import ProductCategory from './pages/ProductCategory'
+import ProductDetails from './pages/ProductDetails'
 
 const App = () => {
 
-const isSellerPath =useLocation().pathname.includes("seller")
+const isSellerPath =useLocation().pathname.includes("seller");
+const {showUserLogin}  =useAppContext();
 
   return (
     <div>
       {isSellerPath ? null:<Navbar />}
+      {showUserLogin ? <Login /> : null}
+
+      <Toaster />
       
       <div className={`${isSellerPath ? "" : "px-6 md:px-16 lg:px-24 xl:px-32"}`}>
         <Routes>
           <Route path="/" element={<Home />} />
-          
+          <Route path="/products" element={<AllProducts />} />
+          <Route path="/products/:Category" element={<ProductCategory />} />
+          <Route path="/products/:Category/:id" element={<ProductDetails />} />
+
         </Routes>
       </div>
+    {!isSellerPath && <Footer/>}
     </div>
   )
 }
--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -1,16 +1,74 @@
 
-import React, { createContext, useState, useContext } from 'react';
+import React, { createContext, useState, useContext, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { dummyProducts } from '../assets/assets';
+import  toast  from 'react-hot-toast';
 
-export const AppContext = createContext();
+export const AppContext = createContext(); 
 
 export const AppContextProvider = ({ children }) => {
+
+    const currency =import.meta.env.VITE_CURRENCY ;
+
     const navigate = useNavigate();
     const [user, setUser] = useState(true);
     const [isSeller, setIsSeller] = useState(false);
     const [showUserLogin, setShowUserLogin] = useState(false);
+    const[products, setProducts] = useState([]);
 
-    const value = { navigate, user, setUser, isSeller, setIsSeller, showUserLogin, setShowUserLogin };
+    const [cartItems, setCartItems] = useState({});
+    const [searchQuery, setSearchQuery] = useState({});
+
+    const fetchProducts = async () => {
+        setProducts(dummyProducts)
+    }
+
+    //add product to cart
+    const addToCart  =(itemId) =>{
+        let cartData =structuredClone(cartItems);
+
+        if(cartData[itemId]){
+            cartData[itemId] += 1;
+        }
+        else{
+            cartData[itemId] = 1;
+        }
+    
+    setCartItems(cartData);
+    toast.success("Added to cart")
+    }
+    //update cart items quantity
+
+    const updateCartItem = () => {
+        let cartData =structuredClone(cartItems);
+        cartData[itemId] = quantity;
+        setCartItems(cartData);
+        toast.success("Cart updated")
+    }
+
+//remove product from cart
+    const removeFromCart = (itemId) => {
+        let cartData = structuredClone(cartItems);
+        if (cartData[itemId]) {
+             cartData[itemId] -=1;
+             if( cartData[itemId] == 0) {
+                delete cartData[itemId];
+            
+                } 
+        }
+            toast.error("Removed from cart");
+            setCartItems(cartData);
+        
+        
+    };
+
+    useEffect(() => {
+        fetchProducts();
+    }, []);
+
+
+
+    const value = { navigate, user, setUser, isSeller, setIsSeller, showUserLogin, setShowUserLogin,products,currency,addToCart,updateCartItem ,removeFromCart,cartItems,searchQuery,setSearchQuery};
 
     return (
         <AppContext.Provider value={value}>
